103619cfee0c28ae9e8bee5f8cdcbcd0fb0e1186
Sakib-01 <syedsakibulhaque2@gmail.com>
2024-10-24 19:01:15 +0600

    add json file , create header

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <div className="body">
+      <Header></Header>
+    </div>
+  );
+}
+
+export default App;
